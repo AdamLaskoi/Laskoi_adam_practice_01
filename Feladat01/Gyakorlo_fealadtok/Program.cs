201732fb0196925433390f560989d42264e96b10
Code Hussar <adam.laskoi@gmail.com>
2023-01-15 00:24:54 +0100

    Gyakorlo_1

---
+++ Feladat01/Gyakorlo_fealadtok/Program.cs
@@ -0,0 +1,40 @@
+﻿
+// 1.feladat
+//Írj programot, ami kiírja a, és b egész (int) számok összegét, különbségét, szorzatát és hányadosát!
+
+/*
+ 
+Console.WriteLine("Kérem az [A] egész számot");
+String intext = Console.ReadLine();
+int numberA = int.Parse(intext);
+Console.WriteLine("Kérem az [B] egész számot");
+       intext = Console.ReadLine();
+int numberB = int.Parse(intext);
+Console.WriteLine("Összege: " + "A + B = " + (numberA + numberB));
+Console.WriteLine("Külömbsége: " + "A - B = " + (numberA - numberB));
+Console.WriteLine("szorzata: " + "A * B = " + (numberA * numberB));
+Console.WriteLine("Hányadosa: " + "A / B = " + (numberA / numberB));
+
+ */
+
+//-----------------------------------------------------------------------
+// 2. feladat
+
+//Írj programot, ami kiírja a, és b egész (int) számok egész hányadosát és osztás utáni maradékát! A bemeneti változókat a konzolról olvasd be!
+
+/*
+Console.WriteLine("Kérem az A számot");
+string inText = Console.ReadLine();
+int number1 = int.Parse(inText);
+Console.WriteLine("Kérem a B számot");
+inText = Console.ReadLine();
+int number2 = int.Parse(inText);
+Console.WriteLine("A Bevitt számok hányadosa: " + (number1 / number2) + " " + "A bevitt számok osztás utáni maradéka: " + (number1 % number2));
+*/
+
+//------------------------------------------------------------------------
+// 3. feladat Átlag
+
+// Írj ami kiszámolja 5 egész szám pontos átlagát és az eredményt kiírja a program. Az 5 egész számot válaszd ki és írd bele a programba!
+
+
